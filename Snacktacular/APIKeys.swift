9b0cc98d5ace9a97b3fdcbb1b75596247e02ea4a
Xiaotong <yuafq@bc.edu>
2022-04-04 14:15:20 -0400

    completed 8.7 google places Autocomplete

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Carol Yu on 4/4/22.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyD-2qLWlLnhlRqKiLaG_DpcHbOyft32AvY"
+}
