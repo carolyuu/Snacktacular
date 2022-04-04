e1540fa368ff256f40e24cc9d81ff623a93f5cdf
Xiaotong <yuafq@bc.edu>
2022-04-04 06:07:57 -0400

    completed 8.5

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,71 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Carol Yu on 4/4/22.
+//
+
+import Foundation
+import Firebase
+class Spot {
+    var name: String
+    var address: String
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    var dictonary: [String: Any] {
+        return ["name": name, "address": address, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    init(name: String, address: String, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    convenience init() {
+        self.init(name: "", address: "", averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        // Grab the user ID
+        guard let postingUserID = Auth.auth().currentUser?.uid else {
+        print("Error: could not save data beucase we don't have a valid postingUserID")
+            return completion(false)
+        }
+        self.postingUserID = postingUserID
+        // create the dictionary representing data we want to save
+        let dataToSave:[String: Any] = self.dictonary
+        // if we HAVE saved a record, we'll have an ID, otherwise .addDocument will create one
+        if self.documentID == "" { // Create a new document via .addDocument
+            var ref: DocumentReference? = nil //Firestore will create new ID for us
+            ref = db.collection("spots").addDocument(data: dataToSave){ (error) in
+                guard error == nil else {
+                    print("Error adding document \(error!.localizedDescription)")
+                    return completion(false)
+                    
+                }
+                self.documentID = ref!.documentID
+                print("💨 Added document: \(self.documentID)") // it worked!
+                completion(true)
+            }
+        } else { // else save to the existing documentID w/ .setData
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                guard error == nil else {
+                    print("😡 Error: updating document \(error!.localizedDescription)")
+                    return completion(false)
+                }
+                print("💨 updated document: \(self.documentID)") // it worked!
+                completion(true)
+            }
+        }
+    }
+}
+
+
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -12,19 +12,48 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var ratingLabel: UILabel!
+    var spot: Spot!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if spot == nil {
+            spot = Spot()
+        }
+        updateUserInterface()
+        
     }
     
-    @IBAction func cancelButtonPressed(_ sender: Any) {
+    func updateUserInterface() {
+        nameTextField.text = spot.name
+        addressTextField.text = spot.address
+    }
+    
+    func updateFromInterface() {
+        spot.name = nameTextField.text!
+        spot.address = addressTextField.text!
+    }
+    
+    func leaveViewController() {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
             dismiss(animated: true, completion: nil)
         } else {
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        updateFromInterface()
+        spot.saveData { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                self.oneButtonAlert(title: "Save Failed", message: "for some reason, the data woule not save t othe cloud")
+            }
+        }
+    }
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+       leaveViewController()
     }
     
     
